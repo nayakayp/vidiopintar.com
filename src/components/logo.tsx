714a6ff4ee5f0f43ee2a1377891ffddb64bf08af
masb0ymas <n.fajri@outlook.com>
2025-09-20 10:38:46 +0700

    fix: update logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link"
+
 export const Logo = () => {
   return (
-    <div className="flex gap-1 items-center">
+    <Link href="/" className="flex gap-1 items-center">
       <img src="play.svg" className="size-5 "></img>
       <span className="select-none tracking-tight">vidiopintar</span>
-    </div>
+    </Link>
   )
 }
