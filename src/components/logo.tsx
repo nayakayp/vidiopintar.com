9ce70b5102ac6b4852201737adb0cb04f3c6f4e8
ahmadrosid <alahmadrosid@gmail.com>
2025-09-26 19:08:42 +0700

    Fix hydration error

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,8 @@
-import Link from "next/link"
-
 export const Logo = () => {
   return (
-    <Link href="/" className="flex gap-1 items-center">
+    <div className="flex gap-1 items-center">
       <img src="play.svg" className="size-5 "></img>
       <span className="select-none tracking-tight">vidiopintar</span>
-    </Link>
+    </div>
   )
 }
